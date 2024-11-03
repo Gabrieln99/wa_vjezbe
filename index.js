29b36a599886d9bf8fea86a21f7b568218c0c824
Gabrieln999 <155887339+Gabrieln99@users.noreply.github.com>
2024-11-03 21:18:58 +0100

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-
 const PORT = 3000;
 
+const users = [
+  { id: 1, ime: "Gabriel", prezime: "Nadal" },
+  { id: 2, ime: "Pero", prezime: "Peric" },
+  { id: 3, ime: "Prof", prezime: "Blaskovic" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+/* ili
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html")); // Koristimo path.join za spajanje putanje
+});
+*/
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/about.html");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.listen(PORT, (error) => {
